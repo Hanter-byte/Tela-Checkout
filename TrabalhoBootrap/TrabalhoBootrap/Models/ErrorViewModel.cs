6f732591daeb45f6e50c892898cf15e78672bc27
João Pedro <jp663687@gmail.com>
2023-04-27 15:52:43 -0300

    first commit

---
+++ TrabalhoBootrap/TrabalhoBootrap/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TrabalhoBootrap.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
